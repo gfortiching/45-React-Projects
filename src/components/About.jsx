41d8e796aea27ce90d34180aa099e0b5b59b7b78
Gab <gabrielfabilena@gmail.com>
2023-01-21 20:39:56 +0800

    Added Project 1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,14 @@ function About() {
     return (
         <>
         <section className="about">
-            <article className="about-section"></article>
+            <article className="about-section">
+                <div className="title"><h4>ABOUT</h4></div>
+                <div className="text">
+                    <p>"Hello! I'm Jose Gabriel F. Fabileña and I am a Full-Stack Web Developer.</p>
+                    <p>I built this website for my React.js challenge.</p>
+                    <p>See you later!"</p>
+                </div>
+            </article>
         </section>
         </>
     )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,31 @@
 import "./css/Projects.css";
+import Counter from "./Project1";
 
 function Projects() {
     return (
         <>
         <section className="projects">
-            <article className="projects-section"></article>
+            <article className="projects-section">
+            <div className="title"><h4>PROJECTS</h4></div>
+                <div className="text">
+                    <p>"Welcome back!</p>
+                    <p>This project will be a work in progress until I complete all the 44 projects.</p>
+                    <p>If you wish to follow me, here is the link:</p>
+                    <a href="https://contactmentor.com/best-react-projects-for-beginners-easy/" target="blank"><p>45 Best React Projects for Beginners in Easy to Hard Order</p></a>
+                    <p>I did not include #1 since I used Vite.js for this website."</p>
+                </div>
+            </article>
+        </section>
+
+        <section className="projects">
+            <article className="projects-section">
+            <div className="title"><h4>PROJECT 1</h4></div>
+                <div className="text">
+                    <h5>Simple Counter</h5>
+                    <h6>Topics covered: React class-based components, props, state.</h6>
+                </div>
+                <Counter />
+            </article>
         </section>
         </>
     )
