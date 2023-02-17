deb816f70d7fdfb3bc3564686da5d4a71b8d19f5
Gab <gabrielfabilena@gmail.com>
2023-02-17 14:03:26 +0800

    Test Deployed

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
-module.exports = {
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
     base: 'https://gfortiching.github.io/vite-portfolio/',
-  }
+    plugins: [react()]
+})
